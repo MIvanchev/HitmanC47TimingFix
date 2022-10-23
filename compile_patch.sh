269e8f69fe89b2973e61a0206307ec3088ee5821
Mihail Ivanchev <contact@ivanchev.net>
2022-10-23 13:27:20 +0200

    Renaming ASM files to GAS extension .s

--- compile_patch.sh
+++ compile_patch.sh
@@ -2,13 +2,13 @@
 
 PATCH_DIR=src/patch
 
-for asm_file in "$PATCH_DIR"/*.asm; do
-  obj_file=${asm_file%.asm}.o
-  hex_file=${asm_file%.asm}.hex
+for asm_file in "$PATCH_DIR"/*.s; do
+  obj_file=${asm_file%.s}.o
+  hex_file=${asm_file%.s}.hex
   module=${asm_file##*/}
   module=${module%%_*}.dll
   address=${asm_file##*_}
-  address=${address%.asm}
+  address=${address%.s}
 
   as -msyntax=intel -mnaked-reg -32 -march=i386+387 -o "$obj_file" "$asm_file" || exit
   ld -melf_i386 --oformat=binary -e 0 -Ttext=0 -o "$hex_file" "$obj_file" || exit
